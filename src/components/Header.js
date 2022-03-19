1c1816c1d05a07b77da1c72a8c17db300a1cedc6
Nurina Junus <nurina.junus@gmail.com>
2022-03-18 22:20:38 -0600

    created components

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>
+            <h1>
+                Gradebook
+                <span className="logo" role="img">
+                ✅ 📕
+                </span>
+            </h1>
+        </header>
+    );
+}
+
+export default Header
